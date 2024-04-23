6ae16f8ac912893351969ca5f32cfd79c20ac1d8
OriReyes <130611288+OriReyes@users.noreply.github.com>
2024-04-23 18:08:59 -0300

    https://github.com/OriReyes/ProyectphpNew.git

---
+++ img/estilos.js
@@ -0,0 +1,11 @@
+/* document.addEventListener('DOMContentLoaded', function() {
+    var boton = document.createElement('span');
+    boton.classList.add('boton-hamburguesa');
+    boton.textContent = '☰';
+    document.body.appendChild(boton);
+  
+    boton.addEventListener('click', function() {
+      var menu = document.querySelector('.menu-principal');
+      menu.classList.toggle('mostrar');
+    });
+  }); */
